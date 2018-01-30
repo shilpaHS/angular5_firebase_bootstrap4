ed370f0e857f4cee4da333f006383a5f40fb87f1
shilpaHS <shilpahs199405@gmail.com>
2018-01-30 16:49:41 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "[...]",
+    authDomain: "[...]",
+    databaseURL: "[...]",
+    projectId: "[...]",
+    storageBucket: "[...]",
+    messagingSenderId: "[...]"
+  }
 };
